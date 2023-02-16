082c7f517121c97c415728f47895f3312d0547f6
raidcam <igor.sinyakov03@gmail.com>
2023-02-16 15:35:45 +0200

    Add CSS to user menu

--- client/src/components/CreateComment.jsx
+++ client/src/components/CreateComment.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import '../styles/App.css'
 const CreateComment = (props) => {
     return (
-        <div>
+        <div className="createSendCommentBlock">
           <form>
-              <input className="inputComment" placeholder="Nickname*"/>
-              <input className="inputComment" placeholder="Your Email*"/>
-              <input className="inputComment" placeholder="Your home page"/>
-              <input className="inputComment" placeholder="Comment* (txt,jpg,png,doc)"/>
+              <input className="inputComment" style={{marginTop: 35}} placeholder="  Nickname*"/>
+              <input className="inputComment" placeholder="  Your Email*"/>
+              <input className="inputComment" placeholder="  Your home page"/>
+              <textarea className="inputCommentField" placeholder="  Comment* (txt,jpg,png,doc)" />
               <button className="createCommentButton">Send comment</button>
           </form>
         </div>

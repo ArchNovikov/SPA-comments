24dd44fb2ed463d5d82a7aa4911aa60c4a028027
Artem Novikov <ArchNovikov@ukr.net>
2023-03-01 14:01:35 +0200

    creating modal form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
+import React, {useEffect, useState} from 'react';
 import Comment from "./components/Comment";
-import React from "react";
+import {fetchComments} from "./http/CommentAPI";
 import CreateComment from "./components/CreateComment";
 
 function App() {
+
+    const [comment, setComment] = useState([]);
+
+    useEffect(() => {
+        fetchComments().then(data => setComment(data))
+    }, [])
+
+
+
+    for (let i = 0; i < comment.length; i++) {
+        comment[i].createdAt = comment[i].createdAt.slice(0, 16).replace('T', '-').split('-').reverse().join('-');
+    }
+
+
     return (
         <div className="App">
-            <Comment name="Arch" text="Hello everybody"/>
-            <Comment name="raidcam" text="Hello Arch"/>
+            {
+                comment.map(comment => (
+                    <Comment name={comment.userId}
+                             date={comment.createdAt}
+                             text={comment.commentText}
+                             key={comment.id}
+                    />))
+            }
+
             <CreateComment/>
         </div>
     );
--- client/src/components/CreateComment.jsx
+++ client/src/components/CreateComment.jsx
@@ -1,44 +1,12 @@
-import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/App.css'
-import Modal from 'react-bootstrap/Modal';
 import CreateCommentForm from "./CreateCommentForm";
 
 const CreateComment = () => {
-
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-
     return (
-
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Create comment
-            </Button>
-
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Creating comment</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <CreateCommentForm/>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
+       <CreateCommentForm/>
     );
-
 
 }
 
